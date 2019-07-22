f53b3cf7fab55b192ec28d2ebb05b68e38b426fb
zzzfairy <zzzfairy@mail.ru>
2019-07-22 17:41:57 +0300

    Make fadeIn and fadeOut, popup open-close

--- js/homework.js
+++ js/homework.js
@@ -1,1 +1,18 @@
-'use strict'
+$(document).ready(function() {
+    $(".main_btna, .main_btn").on("click", function() {
+      $(".overlay").fadeIn("slow");
+      $(".modal").animate(
+          {
+              height: "toggle"
+          }, 3000
+      );
+        });
+      $(".close").on("click", function() {
+        $(".overlay").fadeOut("slow");
+        $(".modal").animate(
+            {
+                height: "toggle"
+            }, 3000
+        );
+      });
+    });
